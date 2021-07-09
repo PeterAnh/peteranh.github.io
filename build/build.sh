4ad748c707a618e8e6348943cafdf6a157245b55
c0mpl3x <peteranh3105@gmail.com>
2021-07-09 22:41:00 +0800

    setup http server with python
    Because .NET doesn't have one T_T

--- build/build.sh
+++ build/build.sh
@@ -10,8 +10,10 @@ app="./build/app/HtmlToPdf"
 
 # Getting stuff
 echo "Intializing..."
+sh -c "python -m http.server 8000" & echo "Running HTTP Server"
 sh -c "./ngrok http 8000" & echo "Running Ngrok"
 echo "Waiting for 5 seconds." && sleep 5
+echo "\nUp and running!\n"
 echo "Building HtmlToPdf..."
 sh -c "dotnet build $project -c Release -o ./build/app/"
 echo "Finish building! Getting PDF..."
